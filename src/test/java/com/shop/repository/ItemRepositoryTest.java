f82fc85723d529469af56684df279e00aaaac69d
nicolao911 <kjk0656@naver.com>
2023-04-18 22:06:10 +0900

    add test case

--- src/test/java/com/shop/repository/ItemRepositoryTest.java
+++ src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -3,6 +3,7 @@ package com.shop.repository;
 import com.shop.constant.ItemSellStatus;
 import com.shop.entity.Item;
 import java.time.LocalDateTime;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,5 +29,68 @@ class ItemRepositoryTest {
         item.setRegTime(LocalDateTime.now());
         Item savedItem = itemRepository.save(item);
         System.out.println(savedItem.toString());
+    }
+
+    // test를 위한 Item list 생성
+    public void createItemList() {
+        for (int i = 0; i <= 10; i++) {
+            Item item = new Item();
+            item.setItemNm("테스트 상품" + i);
+            item.setPrice(10000 + i);
+            item.setItemDetail("테스트 상품 상세 설명" + i);
+            item.setItemSellStatus(ItemSellStatus.SELL);
+            item.setStockNumber(100);
+            item.setRegTime(LocalDateTime.now());
+            Item savedItem = itemRepository.save(item);
+        }
+    }
+
+
+    @Test
+    @DisplayName("상품명 조회 테스트")
+    public void findByItemNmTest() {
+        //Given - 테스트에서 구체화하고자 하는 행동을 시작하기전 테스트 상태를 설명
+        //상품 리스트 생성
+        this.createItemList();
+
+        //When - 구체화 하고자 하는 행동
+        //특정 상품명으로 해당 상품 조회
+        List<Item> itemList = itemRepository.findByItemNm("테스트 상품1");
+
+        //Then - 어떤 특정 행동 때문에 발생할거라고 예상되는 변화에 대한 설명
+        // 해당 상품명으로 조회된 list 출력
+        for (Item item : itemList) {
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("Or test")
+    void findByItemNmOrItemDetailTest() {
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByItemNmOrItemDetail("테스트 상품1", "테스트 상품 상세 설명5");
+        for (Item item : itemList) {
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("lessThen test")
+    void findByPriceLessThan() {
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByPriceLessThan(10005);
+        for (Item item : itemList) {
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("lessThen desc test")
+    void findByPriceLessThanOrderByPriceDesc() {
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByPriceLessThanOrderByPriceDesc(10005);
+        for (Item item : itemList) {
+            System.out.println(item.toString());
+        }
     }
 }
